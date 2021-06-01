c7d2d551ec0cb6874b3bbadab17e1f12ed30fcff
Joe <joeyballesteros.wfg@gmail.com>
2021-06-01 13:53:10 -0700

    router fix again

---
+++ ihfinalproject/src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Switch, Link, Route } from "react-router-dom";
+import Training from "./Training";
+import Home from "./Home";
+
+function Navbar(props) {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          Make Me Pro
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link class="nav-link active" aria-current="page" to="/Home">
+                Home
+              </Link>
+              <Link class="nav-link" to="/Features">
+                Features
+              </Link>
+              <Link class="nav-link" to="/Pricing">
+                Pricing
+              </Link>
+              <Link class="nav-link" to="/Training">
+                Training
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
