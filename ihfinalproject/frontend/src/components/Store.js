f8f5c614e170a776a1bb1431925733fc786955f0
Dani Syku <danisyku@gmail.com>
2021-06-02 01:24:29 -0400

    carousel

--- ihfinalproject/frontend/src/components/Store.js
+++ ihfinalproject/frontend/src/components/Store.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import HeaderStore from './HeaderStore';
+import {carousel} from 'react-bootstrap';
 
 function Store(props) {
     return (
         <div>
-            Here is our Store
+         Here is our Store
+         <HeaderStore/>
+           
         </div>
+       
     );
 }
 
