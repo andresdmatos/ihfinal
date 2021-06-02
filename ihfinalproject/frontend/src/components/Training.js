e80397863872e7c3b9fcbc567a79111cf561f21b
Joe <joeyballesteros.wfg@gmail.com>
2021-06-02 08:31:08 -0700

    video player

--- ihfinalproject/frontend/src/components/Training.js
+++ ihfinalproject/frontend/src/components/Training.js
@@ -1,7 +1,38 @@
 import React from "react";
+import ReactPlayer from "react-player";
+
+const links = [
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+];
 
 function Training(props) {
-  return <div>We Want To Train!</div>;
+  const [readyCount, setReadyCount] = React.useState(0);
+  const [playing, setPlaying] = React.useState(false);
+
+  const onReady = () => {
+    setReadyCount(readyCount + 1);
+  };
+
+  React.useEffect(() => {
+    if (readyCount === links.length) {
+      setPlaying(true);
+    }
+  }, [readyCount]);
+
+  return (
+    <div>
+      {links.map((url) => (
+        <ReactPlayer
+          key={url}
+          playing={playing}
+          onReady={onReady}
+          url={url}
+          controls
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Training;
