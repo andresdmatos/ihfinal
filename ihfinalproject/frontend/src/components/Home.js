466d90587d87c2a0b46a117fa5e90353856a21b3
Joe <joeyballesteros.wfg@gmail.com>
2021-06-08 11:01:28 -0700

    updated home

--- ihfinalproject/frontend/src/components/Home.js
+++ ihfinalproject/frontend/src/components/Home.js
@@ -4,26 +4,32 @@ import womansolohit from "../assets/womansolohit.png";
 
 function Home(props) {
   return (
-    <div>
+    <div className="py-5 text-center container">
       <div class="container-fluid">
-        FAMER'S STORY Great coaching is all about personal attention. The more
-        personal the coach gets, the better the student becomes. But personal
-        coaching is expensive and coaches can’t give their full personal
-        attention to many trainees at the same time. Famer is an affordable
-        personalized communication coaching platform designed to support coaches
-        in sharing their knowledge and expertise at scale, yet on a personal
-        level. Regularly using Famer encourages training beyond official class
-        time and allows coaches to empower trainees with a constant presence of
-        a mentor in their lives.
+        <div className="card-body">
+          <h1 className="card-header">MAKE ME PRO</h1>
+          FAMER'S STORY Great coaching is all about personal attention. The more
+          personal the coach gets, the better the student becomes. But personal
+          coaching is expensive and coaches can’t give their full personal
+          attention to many trainees at the same time. Famer is an affordable
+          personalized communication coaching platform designed to support
+          coaches in sharing their knowledge and expertise at scale, yet on a
+          personal level. Regularly using Famer encourages training beyond
+          official class time and allows coaches to empower trainees with a
+          constant presence of a mentor in their lives.
+        </div>
       </div>
       <div class="container-fluid">
         <div className="row">
-          <div className="col-lg-6  col-sm-12 card-body leftTextCard">
-            <h3>Our Vision</h3>
-            To empower coaches, teachers, and trainers to build personal,
-            meaningful relationships with trainees at scale, thus maximizing
-            their potential.
+          <div className="col-lg-6  col-sm-12 card-body d-flex align-items-center">
+            <div>
+              <h3>Our Vision</h3>
+              To empower coaches, teachers, and trainers to build personal,
+              meaningful relationships with trainees at scale, thus maximizing
+              their potential.
+            </div>
           </div>
+
           <div className="col-lg-6 col-sm-12">
             <div className="card text-left">
               <img src={volleynetdoubles} alt="silhoutte" />
@@ -33,16 +39,18 @@ function Home(props) {
       </div>
       <div className="container-fluid row">
         <div className="row">
-          <div className="col-lg-6  col-sm-12 card-body rightTextCard">
+          <div className="col-lg-6  col-sm-12 card-body">
             <div className="card text-left">
               <img src={womansolohit} alt="silhoutte" />
             </div>
           </div>
-          <div className="col-lg-6 col-sm-12">
-            <h3>Our Mission</h3>
-            To empower coaches, teachers, and trainers to build personal,
-            meaningful relationships with trainees at scale, thus maximizing
-            their potential.
+          <div className="col-lg-6 col-sm-12  d-flex align-items-center">
+            <div>
+              <h3>Our Mission</h3>
+              To empower coaches, teachers, and trainers to build personal,
+              meaningful relationships with trainees at scale, thus maximizing
+              their potential.
+            </div>
           </div>
         </div>
       </div>
